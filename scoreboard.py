8070021135746f88c9795a2f7658101933d24703
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-01-12 08:02:55 -0800

    create data.txt to keep track of the high score. modified scoreboard.py to read and write from/to data.txt.

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -32,5 +33,5 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.clear()
         self.write(f"Score: {self.score} | High Score: {self.high_score}", align=BOARD_ALIGNMENT, font=TEXT_FONT)
-
-
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
